15e72537975a334195d05077f4a2136651c93191
Kevzys <kevinamehta@hotmail.com>
2019-10-15 15:01:13 -0700

    cleaned up files

--- test.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-call = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPAAAL&apikey=6H75Z4MUU2EOD3H1'
-data = response.get(call)
-print(call)
